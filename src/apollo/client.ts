ac4ac4c62ad19df080bb06cfea5242cf820e50e9
Sebastian Pozo <sebastian.pozol14@gmail.com>
2026-04-14 01:06:12 -0500

    Update Project version 1.3

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/api/graphql',
+  uri: `${import.meta.env.VITE_API_URL}/api/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
